5c3463c561c602077559b47c747894f2cf90bfbb
Yedidya <didihazan123@gmail.com>
2024-08-07 13:47:06 +0300

    update files

--- src/pages/MobileContent.js
+++ src/pages/MobileContent.js
@@ -3,9 +3,31 @@ import '../CSS/MobileContent.css';
 import logo from "../images/logo.png";
 import RandomStars from "../components/RandomStars";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faCar, faPaperPlane, faStar} from "@fortawesome/free-solid-svg-icons";
+import {faCar, faClock, faPaperPlane, faStar} from "@fortawesome/free-solid-svg-icons";
+import { useLongPress } from 'use-long-press';
+
 
 const MobileContent = () => {
+    const handleLongPress = () => {
+        const messageElement = document.getElementById('travelModeMessage');
+        if (messageElement) {
+            messageElement.style.display = 'block';
+            setTimeout(() => {
+                messageElement.style.display = 'none';
+            }, 3000); // מציג את ההודעה למשך 3 שניות
+        }
+    };
+
+    const longPressEvent = useLongPress(handleLongPress, {
+        onCancel: () => {
+            const messageElement = document.getElementById('travelModeMessage');
+            if (messageElement) {
+                messageElement.style.display = 'none';
+            }
+        }
+    });
+
+
     return (
         <div className="mobile-container">
             <div className="mobile-home">
@@ -21,7 +43,7 @@ const MobileContent = () => {
                 <RandomStars numStars={6}/>
                 <FontAwesomeIcon icon={faStar} size="1x" style={{color: "#ffffff"}}/>
                 <div className="mobile-curved-background">
-                    <div className="mobile-logo-container">
+                    <div className="mobile-logo-container" {...longPressEvent}>
                         <img src={logo} alt="KidiSafe-logo" className="mobile-main-img"/>
                     </div>
                     <h1>KidiSafe</h1>
@@ -29,10 +51,9 @@ const MobileContent = () => {
                     <div className="button-container">
                         <div className="button-wrapper">
                             <button className="round-button">
-                                <FontAwesomeIcon icon={faCar} flip="horizontal" size="2x" style={{color: "#59cfd3"}}
-                                                 className="round-icon"/>
+                                <FontAwesomeIcon icon={faClock} size="2x" style={{color: "#59cfd3"}} className="round-icon"/>
                             </button>
-                            <span className="button-label">רכב 1</span>
+                            <span className="button-label">התאם זמני נסיעה</span>
                         </div>
                         <div className="button-wrapper">
                             <button className="round-button">
